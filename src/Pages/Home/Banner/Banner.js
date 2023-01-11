18446a8656891655399a8e084a8244317c9a66f3
JubaStriker <jubastriker@gmail.com>
2023-01-12 02:18:55 +0600

    react-icons installed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,12 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import food1 from '../../../Assets/food1.jpg'
-import food2 from '../../../Assets/food2.jpg'
+import food3 from '../../../Assets/Breakfast2.jpg'
+import food4 from '../../../Assets/Dinner.jpg'
+import food5 from '../../../Assets/Festive.jpg'
+import food6 from '../../../Assets/Festive2.jpg'
+import food7 from '../../../Assets/Birthday.jpg'
+import food8 from '../../../Assets/Breakfast.jpg'
 
 
 const Banner = () => {
@@ -27,8 +32,12 @@ const Banner = () => {
                     className="mySwiper"
                 >
                     <SwiperSlide><img src={food1} alt="" /></SwiperSlide>
-                    <SwiperSlide><img src={food2} alt="" /></SwiperSlide>
-                    <SwiperSlide><img src={food1} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={food3} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={food4} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={food5} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={food6} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={food7} alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={food8} alt="" /></SwiperSlide>
                 </Swiper>
             </>
         </div>
--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import About from "../../Pages/About/About";
+import Blog from "../../Pages/Blog/Blog";
+import Contact from "../../Pages/Contact/Contact";
 import Home from "../../Pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
@@ -13,7 +16,20 @@ export const router = createBrowserRouter([
         {
             path: '/home',
             element: <Home />
-        }]
+        },
+        {
+            path: '/blog',
+            element: <Blog />
+        },
+        {
+            path: '/about',
+            element: <About />
+        },
+        {
+            path: '/contact',
+            element: <Contact />
+        }
+        ]
 
     }
 ])
