bd3ec37b8954c8aacbd813073055e5759cf08cfe
Talimul212 <islamtalimul0@gmail.com>
2023-01-12 22:39:06 +0600

    food Section font size and color

--- src/Pages/Home/FoodItem/FoodItem.js
+++ src/Pages/Home/FoodItem/FoodItem.js
@@ -12,10 +12,10 @@ const FoodItem = () => {
         <div className='mx-10 my-10'>
         <div>
             <div className='text-center mb-4'>
-                <p className="text-6xl font-bold text-yellow-600">Top Foods</p>
-                <p className="text-2xl text-yellow-600 my-7">More Than <span className='text-red-600'>10,000</span> Items To Orders!</p>
+                <p className="text-2xl  font-bold text-yellow-600">Top Foods</p>
+                <p className="text-yellow-600 mt-3 mb-7 text-4xl">More Than <span className='text-red-600'>10,000</span> Items To Orders!</p>
             </div>
-            <div className='p-5 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 '>
+            <div className='gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 '>
             {
                         foodItems.map(foodItem => <FoodItemCard
                             key={foodItem.id}
--- src/Pages/Home/FoodItem/FoodItemCard.js
+++ src/Pages/Home/FoodItem/FoodItemCard.js
@@ -3,14 +3,14 @@ import { FaPlusCircle,FaRegArrowAltCircleRight } from "react-icons/fa";
 const FoodItemCard = ({ foodItem }) => {
   const { id, img, price, title, description } = foodItem;
   return (
-    <div>
-      <div className=" hero rounded-2xl " style={{ backgroundImage: `url(${img})`,height:"380px" }} >
-        <div className="card-body rounded-2xl hero-overlay bg-opacity-30">
-          <h2 className="card-title text-white">{title}</h2>
-          <p className='text-amber-300 font-semibold'>{description}</p>
+    <div className=''>
+      <div className=" hero rounded-2xl shadow-xl " style={{ backgroundImage: `url(${img})`,height:"380px" }} >
+        <div className="card-body rounded-2xl shadow-xl hero-overlay bg-opacity-30">
+          <h2 className="card-title text-3xl text-white">{title}</h2>
+          <p className='text-amber-300 font-semibold text-xl'>{description}</p>
           <div className="card-actions justify-end">
-            <p className='text-amber-300 font-semibold'>$ {price}</p>
-         <FaRegArrowAltCircleRight className=' text-amber-400 hover:text-white text-3xl'></FaRegArrowAltCircleRight>
+            <p className='text-amber-300 font-semibold text-xl'>$ {price}</p>
+         <FaRegArrowAltCircleRight className=' text-amber-400 hover:text-white text-4xl'></FaRegArrowAltCircleRight>
           </div>
         </div>
       </div>
