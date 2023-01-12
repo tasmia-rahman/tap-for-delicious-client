74739df6cf3806bbcaa8fd6693feee82dc5641c6
mostakshaikh50 <mostak.50.cse@gmail.com>
2023-01-12 07:56:41 +0000

    blog ad footer section added

--- src/Pages/Home/Blog/Blog.js
+++ src/Pages/Home/Blog/Blog.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BlogCard from './BlogCard';
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect( () => {
+        fetch('blog.json')
+        .then(res => res.json()
+        .then(data => setBlogs(data)))
+    }, [])
     return (
         <div>
-            <h3>This is Blog</h3>
+            <div>
+            <div className='text-center mb-4'>
+                <p className="text-6xl font-bold text-yellow-600">Our Latest Blogs</p>                
+            </div>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    blogs.map(blog => <BlogCard
+                      key ={blog.id}
+                      blog ={blog}
+                    ></BlogCard>)
+                }                
+            </div>
+            
+        </div>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Blog from '../Blog/Blog';
+import Footer from '../../Shared/Footer/Footer';
 
 const Home = () => {
     return (
         <div className='min-h-screen'>
             <div>
                 <Banner />
-                
+                <Blog></Blog>
+                <Footer></Footer>
             </div>
         </div>
     );
