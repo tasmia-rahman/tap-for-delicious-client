2134f52ec38cf99c6cc8ebfc96d7418d30cb58a9
Mohammod Ali <mohammodali651@gmail.com>
2023-01-12 19:29:05 +0600

    client choice section done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Blog from '../Blog/Blog';
 import Footer from '../../Shared/Footer/Footer';
 import FoodItem from '../FoodItem/FoodItem';
+import ClientChoice from '../ClientChoice/ClientChoice';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
                 <Banner />
                 <FoodItem></FoodItem>
                 <Blog></Blog>
+                <ClientChoice></ClientChoice>
                 <Footer></Footer>
             </div>
         </div>
