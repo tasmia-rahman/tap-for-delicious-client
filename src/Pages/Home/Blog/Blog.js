f5dff4b1ef0cd322eb5b256aa36a9878f5667bdc
Mohammod Ali <mohammodali651@gmail.com>
2023-01-12 23:33:35 +0600

    client features error solved

--- src/Pages/Home/Blog/Blog.js
+++ src/Pages/Home/Blog/Blog.js
@@ -3,27 +3,27 @@ import BlogCard from './BlogCard';
 
 const Blog = () => {
     const [blogs, setBlogs] = useState([]);
-    useEffect( () => {
+    useEffect(() => {
         fetch('blog.json')
-        .then(res => res.json()
-        .then(data => setBlogs(data)))
+            .then(res => res.json()
+                .then(data => setBlogs(data)))
     }, [])
     return (
         <div>
             <div>
-            <div className='text-center mb-4'>
-                <p className="text-6xl font-bold text-yellow-600">Our Latest Blogs</p>                
+                <div className='text-center mb-4'>
+                    <p className="text-6xl font-bold text-yellow-600">Our Latest Blogs</p>
+                </div>
+                <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                    {
+                        blogs.map(blog => <BlogCard
+                            key={blog.id}
+                            blog={blog}
+                        ></BlogCard>)
+                    }
+                </div>
+
             </div>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                {
-                    blogs.map(blog => <BlogCard
-                      key ={blog.id}
-                      blog ={blog}
-                    ></BlogCard>)
-                }                
-            </div>
-            
-        </div>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,14 +3,23 @@ import Banner from '../Banner/Banner';
 import Blog from '../Blog/Blog';
 import Footer from '../../Shared/Footer/Footer';
 import FoodItem from '../FoodItem/FoodItem';
+import Reviews from '../Reviews/Reviews';
 import ClientChoice from '../ClientChoice/ClientChoice';
+import {BiSearchAlt2} from 'react-icons/bi'
 
 const Home = () => {
     return (
         <div className='min-h-screen'>
             <div>
-                <Banner />
+                <div className='relative'>
+                    <Banner />
+                    <div className='absolute top-20 left-[45%] z-30 flex items-center gap-3'>
+                        <input type="text" placeholder="Search" className="input input-bordered input-error w-full max-w-xs" />
+                        <div><BiSearchAlt2 className='text-white text-3xl hover:text-4xl' /></div>
+                    </div>
+                </div>
                 <FoodItem></FoodItem>
+                <Reviews></Reviews>
                 <Blog></Blog>
                 <ClientChoice></ClientChoice>
                 <Footer></Footer>
