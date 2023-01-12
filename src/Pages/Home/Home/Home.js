134924c36023fd031f033a8801d34f8641459619
Mohammod Ali <mohammodali651@gmail.com>
2023-01-12 23:42:36 +0600

    client feature added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,9 @@ import Banner from '../Banner/Banner';
 import Blog from '../Blog/Blog';
 import Footer from '../../Shared/Footer/Footer';
 import FoodItem from '../FoodItem/FoodItem';
+import Reviews from '../Reviews/Reviews';
+import ClientChoice from '../ClientChoice/ClientChoice';
+import {BiSearchAlt2} from 'react-icons/bi'
 
 const Home = () => {
     return (
