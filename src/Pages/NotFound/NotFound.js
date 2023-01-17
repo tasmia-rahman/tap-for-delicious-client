853d59c71e47e61caf65158c07e8b63deecad1ef
JubaStriker <jubastriker@gmail.com>
2023-01-17 22:23:41 +0600

    lottie file added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,23 +1,23 @@
+import { Player } from "@lottiefiles/react-lottie-player";
 import React from "react";
-import Lottie from "react-lottie";
-import data from "./ssssttt-shut-up-the-cat-is-sleeping.json";
 
 const NotFound = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: data,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
   return (
     <div>
       <div className="text-center mt-10">
         <h1 className="font-semibold text-5xl">Ops...</h1>
         <h1 className="text-4xl ">Page not Found.</h1>
       </div>
-      <Lottie options={defaultOptions} height={400} width={400} />
+      <div className="max-w-[400px] mx-auto my-20">
+        <Player
+          src='https://assets8.lottiefiles.com/packages/lf20_suhe7qtm.json'
+          className="player"
+          loop
+          autoplay
+          speed={1}
+        />
+      </div>
+
     </div>
   );
 };
