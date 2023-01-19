d4205414cccab1ff4984eec226cbacf8b5d334a2
tasmia-rahman <maliha1288@gmail.com>
2023-01-20 02:55:24 +0600

    restaurant registration backend done

--- src/Pages/RestaurantRegistration/RestaurantRegistration.js
+++ src/Pages/RestaurantRegistration/RestaurantRegistration.js
@@ -1,18 +1,63 @@
 import React from "react";
+import { toast } from "react-hot-toast";
+import { useNavigate } from 'react-router-dom';
 
 const RestaurantRegistration = () => {
 
-    const handleRestaurantReg = (event) => {
-        event.preventDefault()
-        const form = event.target;
-        const name = form.name.value;
-        const restaurant = form.restaurant.value;
-        const address = form.address.value;
-        const email = form.email.value;
-        const phone = form.phone.value;
-        const img = form.picture.value;
-        console.log(name, restaurant, address, email, phone)
-    }
+  const navigate = useNavigate();
+  const imageHostKey = process.env.REACT_APP_imgbb_key;
+
+  const handleRestaurantReg = (event) => {
+    event.preventDefault()
+    const form = event.target;
+    const name = form.name.value;
+    const restaurantName = form.restaurantName.value;
+    const address = form.address.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const image = form.image.files[0];
+    console.log(image);
+
+    const formData = new FormData();
+    formData.append('image', image);
+    const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.json())
+      .then(imgData => {
+        if (imgData.success) {
+          console.log(imgData.data.url);
+
+          const seller = {
+            image: imgData.data.url,
+            name,
+            restaurantName,
+            address,
+            email,
+            phone
+          }
+          // save seller information to the database
+          fetch('http://localhost:5000/sellers', {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json'
+            },
+            body: JSON.stringify(seller)
+          })
+            .then(res => res.json())
+            .then(result => {
+              console.log(result);
+              if (result.acknowledged) {
+                toast.success('Registered Successfully.');
+                navigate('/dashboard/seller');
+              }
+            })
+        }
+      })
+
+  }
 
   return (
     <div className="mb-20">
@@ -35,7 +80,7 @@ const RestaurantRegistration = () => {
           </label>
           <input
             type="text"
-            name="restaurant"
+            name="restaurantName"
             placeholder="Type restaurant name"
             className="input input-bordered input-warning w-full max-w-xs"
           />
@@ -67,7 +112,7 @@ const RestaurantRegistration = () => {
             <span className="label-text">What is your phone number?</span>
           </label>
           <input
-            type="number"
+            type="text"
             name="phone"
             placeholder="Type your phone number"
             className="input input-bordered input-warning w-full max-w-xs"
@@ -78,10 +123,9 @@ const RestaurantRegistration = () => {
             <span className="label-text">Picture upload</span>
           </label>
           <input type="file"
-          name="picture"
-          className="file-input file-input-bordered file-input-warning w-full max-w-xs" />
+            name="image"
+            className="file-input file-input-bordered file-input-warning w-full max-w-xs" />
         </div>
-        
 
         <button className="btn btn-outline btn-warning max-w-xs block mx-auto mt-5" type="submit">Submit</button>
       </form>
