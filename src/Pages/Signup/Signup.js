8df6a0cb8511dae19531d441b0393828144cd671
JubaStriker <jubastriker@gmail.com>
2023-01-19 16:38:31 +0600

    signup page error fixed

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import logo from '../../Assets/tap-logo.png'
-import {Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { signupInitiate } from '../../Redux/action';
 
 const Signup = () => {
@@ -11,32 +11,32 @@ const Signup = () => {
         password: "",
         passwordConfirm: ""
     });
-    
+
     const { currentUser } = useSelector(state => state.user);
 
     const navigate = useNavigate();
 
-    useEffect(() =>{
-        if(currentUser){
+    useEffect(() => {
+        if (currentUser) {
             navigate('/');
         }
     }, [currentUser, navigate])
 
     const dispatch = useDispatch();
 
-    const {email, password, displayName, passwordConfirm} = state;
+    const { email, password, displayName, passwordConfirm } = state;
     const handleSignup = (event) => {
         event.preventDefault();
-        if( password !== passwordConfirm){
+        if (password !== passwordConfirm) {
             return;
         }
         dispatch(signupInitiate(email, password, displayName));
-        setState({email: "", displayName: "", password: "", passwordConfirm: ""})
+        setState({ email: "", displayName: "", password: "", passwordConfirm: "" })
     };
 
-    const handleChange = (e) =>{
-        let {name, value} = e.target;
-        setState({...state, [name]: value})
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+        setState({ ...state, [name]: value })
     };
 
 
@@ -67,7 +67,7 @@ const Signup = () => {
                                 clipRule="evenodd" />
                         </svg>
                         <input className="pl-2 outline-none border-none" type="text" name="displayName" id="" placeholder="Full name" onChange={handleChange} value={displayName} required />
-                    </div>                    
+                    </div>
                     <div className="flex items-center border-2 hover:border-yellow-400 py-2 px-3 rounded-2xl mb-4">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor">
@@ -84,7 +84,7 @@ const Signup = () => {
                                 clipRule="evenodd" />
                         </svg>
                         <input className="pl-2  border-none outline-none"
-                            type="password" name="password" id="" placeholder="Password" onChange={handleChange} value={password} required/>
+                            type="password" name="password" id="" placeholder="Password" onChange={handleChange} value={password} required />
                     </div>
                     <div className="flex items-center border-2 hover:border-yellow-400 focus:border-yellow-400 py-2 px-3 rounded-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
@@ -94,15 +94,15 @@ const Signup = () => {
                                 clipRule="evenodd" />
                         </svg>
                         <input className="pl-2  border-none outline-none"
-                            type="password" name="passwordConfirm" id="" placeholder="Repeat Password" onChange={handleChange} value={passwordConfirm} required/>
+                            type="password" name="passwordConfirm" id="" placeholder="Repeat Password" onChange={handleChange} value={passwordConfirm} required />
                     </div>
                     <button type="submit" className="block w-full  mt-4 py-2 rounded-2xl font-semibold mb-2 btn mr-10 border-2 border-amber-400 bg-transparent text-amber-500
-                hover:bg-amber-400 hover:text-white hover:border-white text">Sign Up</button>                    
+                hover:bg-amber-400 hover:text-white hover:border-white text">Sign Up</button>
                     <div>
                         <span className="text-sm ml-2 hover:text-yellow-500 cursor-pointer">Already on Tap for Delicious?</span>
                         <Link to="/login"> Log In</Link>
 
-                    </div>                    
+                    </div>
                 </form>
             </div>
         </div>
