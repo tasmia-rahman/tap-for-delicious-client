7cf20277d2ceca1add718a5227770f54a6259776
abrahon <mdabrahon926@gmail.com>
2023-01-19 23:10:58 +0600

    added food item of restaurant

--- src/Pages/Home/AllCategory/AllCategoryDetails.js
+++ src/Pages/Home/AllCategory/AllCategoryDetails.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
 
-const AllCategoryDetails = ({restaurant}) => {
-    const {image, name, description, price} = restaurant;
-  
+const AllCategoryDetails = ({ restaurant }) => {
+    const { image, name, details, price } = restaurant;
+
+
 
     return (
-        <div>
-             <h1 className="text-3xl">This is AllCategory  Page</h1>
-        
-              <div className='grid lg:grid-cols-2 w-full p-10'>
-                <div className='m-10'>
-                    <img className='mx-auto w-full h-full rounded-2xl' src={image} alt="" />
-                </div>
-                <div>
-                    <h2 className='text-3xl font-bold text-orange-600 m-5'>{name}</h2>
-                    <p>{description}</p>
-                    <p className='text-2xl text-orange-600 font-semibold'>Price: ${price} </p>
 
+        <div className='h-[220px] flex justify-center items-center drop-shadow-2xl'>
+            <div className='flex gap-5 justify-start shadow-xl   w-[600px] '>
+                <div className='w-25'>
+                    <div className='mask w-24 h-24'>
+
+                        <img className=' mx-0' src={image} alt="" />
+
+                    </div>
                 </div>
+                <div className='w-75'>
+                    <h2 className='text-3xl font-bold '>{name}</h2>
+                    <p>{details}</p>
+                    <p className=' text-red-600 font-semibold'>Price: {price}</p>
+                    <div className="card-actions justify-end p-3">
+                    <button className="btn btn-warning">Add to card</button>
+                </div>
+                </div>
+                
             </div>
         </div>
     );
