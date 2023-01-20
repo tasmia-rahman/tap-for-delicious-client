967526fdd641f793d9b3990df77189bfc3b71e15
abrahon <mdabrahon926@gmail.com>
2023-01-20 18:05:16 +0600

    added modify food items

--- src/Pages/Home/AllCategory/AllCategory.js
+++ src/Pages/Home/AllCategory/AllCategory.js
@@ -11,10 +11,12 @@ const AllCategory = () => {
           
            <div>
            <img className='h-2/4' src={restaurants.img} alt="" />
-            <h1 className='text-3xl font-bold'>{restaurants.title}</h1>
+           <h1 className='text-3xl font-bold mx-5 '>{restaurants.title}</h1>
+            <p className='text-xl mx-5'>{restaurants.location}</p>
            </div>
            
-            {
+           <div className='mt-15'>
+           {
                 restaurants?.item.map(restaurant=><AllCategoryDetails
                
                 restaurant={restaurant}
@@ -23,6 +25,7 @@ const AllCategory = () => {
                 
               
             }
+           </div>
            
            
         </div>
