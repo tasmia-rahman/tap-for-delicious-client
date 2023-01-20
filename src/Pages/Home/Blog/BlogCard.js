327437bc213cc2ea0f43df0fc0713e9a98d05f9a
tasmia-rahman <maliha1288@gmail.com>
2023-01-20 18:24:44 +0600

    blogcard error solved

--- src/Pages/Home/Blog/BlogCard.js
+++ src/Pages/Home/Blog/BlogCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 
 const BlogCard = ({ blog }) => {
-    const { img, title, description } = blog;
+    const { img, title, details } = blog;
     return (
         <div className="card card-compact w-full lg:max-w-96 bg-base-100 shadow-xl">
             <div>
@@ -11,7 +11,7 @@ const BlogCard = ({ blog }) => {
             </div>
             <div className="card-body">
                 <h2 className="text-2xl font-semibold text-center">{title}</h2>
-                <p className='text-lg'>{description.slice(0, 100)}....
+                <p className='text-lg'>{details.slice(0, 100)}....
                     <Link to='/blog'>
                         <span className='font-semibold hover:text-yellow-400 cursor-pointer ml-2'>read more</span>
                     </Link>
