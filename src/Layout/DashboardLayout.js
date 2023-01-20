d6c115368c8854dc7eb8da86547226ed78729873
tasmia-rahman <maliha1288@gmail.com>
2023-01-20 17:28:19 +0600

    seller dashboard and add blog done

---
+++ src/Layout/DashboardLayout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import DashboardNavbar from '../Pages/Shared/DashboardNavbar/DashboardNavbar';
+
+const DashboardLayout = () => {
+    return (
+        <>
+            <DashboardNavbar></DashboardNavbar>
+            <div className="drawer drawer-mobile">
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content pt-7 px-8">
+                    <Outlet></Outlet>
+                    <label htmlFor="dashboard-drawer" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                </div>
+                <div className="drawer-side drawer-container">
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 text-base-content">
+                        <li><Link className='underline' to="/dashboard/addBlog">Add Blog</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default DashboardLayout;
--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -3,6 +3,7 @@ import Main from "../../Layout/Main";
 import About from "../../Pages/About/About";
 import Blog from "../../Pages/Blog/Blog";
 import Contact from "../../Pages/Contact/Contact";
+import AddBlog from "../../Pages/Dashboard/Seller/AddBlog/AddBlog";
 import AllCategory from "../../Pages/Home/AllCategory/AllCategory";
 import Home from "../../Pages/Home/Home/Home";
 import TopRestaurant from "../../Pages/Home/TopRestaurant/TopRestaurant";
@@ -12,64 +13,79 @@ import RestaurantRegistration from "../../Pages/RestaurantRegistration/Restauran
 import AvailableRestaurant from "../../Pages/Shared/TopFoodDetail/AvailableRestaurant/AvailableRestaurant";
 import Signup from "../../Pages/SignUp/Signup";
 import Wishlist from "../../Pages/Wishlist/Wishlist";
+import DashboardLayout from './../../Layout/DashboardLayout';
+import Dashboard from './../../Pages/Dashboard/Dashboard/Dashboard';
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Main />,
         errorElement: <NotFound />,
-        children: [{
-            path: '/',
-            element: <Home />
-        },
-        {
-            path: '/home',
-            element: <Home />
-        },
-        {
-            path: '/blog',
-            element: <Blog />
-        },
-        {
-            path: '/about',
-            element: <About />
-        },
-        {
-            path: '/contact',
-            element: <Contact />
-        },
-        {
-            path: '/toprestaurant',
-            element: <TopRestaurant />
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/home',
+                element: <Home />
+            },
+            {
+                path: '/blog',
+                element: <Blog />
+            },
+            {
+                path: '/about',
+                element: <About />
+            },
+            {
+                path: '/contact',
+                element: <Contact />
+            },
+            {
+                path: '/toprestaurant',
+                element: <TopRestaurant />
 
-        },
-        {
-            path: '/allcategory/:id',
-            element:<AllCategory></AllCategory>,
-            loader: ({params})=>fetch(`http://localhost:5000/services/${params.id}`)
-        },
-        {
-            path: '/login',
-            element: <Login />
-        },
-
-        {
-            path: '/details',
-            element: <AvailableRestaurant />
-        },
-        {
-            path: '/restaurantReg',
-            element: <RestaurantRegistration></RestaurantRegistration>
-        },
-
-        {
-            path: '/wishlist',
-            element: <Wishlist />
-        },
-        {
-            path: '/signup',
-            element: <Signup />
-        }
+            },
+            {
+                path: '/allcategory/:id',
+                element: <AllCategory></AllCategory>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: '/details',
+                element: <AvailableRestaurant />
+            },
+            {
+                path: '/restaurantReg',
+                element: <RestaurantRegistration></RestaurantRegistration>
+            },
+            {
+                path: '/wishlist',
+                element: <Wishlist />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            }
         ]
-
+    },
+    {
+        path: '/dashboard',
+        element: <DashboardLayout></DashboardLayout>,
+        errorElement: <NotFound></NotFound>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Dashboard></Dashboard>
+            },
+            {
+                path: '/dashboard/addBlog',
+                element: <AddBlog></AddBlog>
+            }
+        ]
     }
 ])
