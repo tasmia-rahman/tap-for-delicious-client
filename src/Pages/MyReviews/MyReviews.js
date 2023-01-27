7b6e7dc143e76782f7bc35b43fcac5ac72f1abf0
abrahon <mdabrahon926@gmail.com>
2023-01-27 18:12:55 +0600

    added review section

---
+++ src/Pages/MyReviews/MyReviews.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const MyReviews = () => {
+    const[myReview, setMyReview] = useState([]);
+
+    useEffect(()=>{
+        fetch(``)
+
+    },[])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyReviews;
