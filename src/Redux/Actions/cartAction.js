0b673c50fb16f675ff6961dbea10aaf256747a85
tasmia-rahman <maliha1288@gmail.com>
2023-02-01 01:35:04 +0600

    cart feature done

--- src/Redux/Actions/cartAction.js
+++ src/Redux/Actions/cartAction.js
@@ -1,12 +1,14 @@
 import { ADD_TO_CART, DELETE_FROM_CART } from './../ActionTypes/cartActionTypes';
 
-export const addToCart = (foodItem) => (dispatch, getState) => {
+export const addToCart = (foodItem, quantity) => (dispatch, getState) => {
     let cartItem = {
         _id: foodItem._id,
         image: foodItem.image,
         name: foodItem.name,
         details: foodItem.details,
-        price: foodItem.price
+        price: foodItem.price,
+        quantity,
+        totalPrice: (foodItem.price * quantity)
     }
     dispatch({ type: ADD_TO_CART, payload: cartItem });
     localStorage.setItem('cartItems', JSON.stringify(getState().cartReducer.cartItems))
--- src/Redux/Reducers/cartReducer.js
+++ src/Redux/Reducers/cartReducer.js
@@ -3,17 +3,29 @@ import { ADD_TO_CART, DELETE_FROM_CART } from './../ActionTypes/cartActionTypes'
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case ADD_TO_CART:
-            return {
-                ...state,
-                cartItems: [...state.cartItems, action.payload]
+            const alreadyExists = state.cartItems.find(
+                (item) => item.name === action.payload.name
+            );
+            if (alreadyExists) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(item => item.name === action.payload.name ? action.payload : item)
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, action.payload]
+                }
             }
+
         case DELETE_FROM_CART:
             return {
                 ...state,
                 cartItems: state.cartItems.filter((item) => item.name !== action.payload.name)
             }
+
         default:
-            return state
+            return state;
     }
 }
 
