5edff6703bc62640851b2545ac3cf8e43a84111a
abrahon <mdabrahon926@gmail.com>
2023-02-03 18:12:25 +0600

    added food details page

--- src/Pages/Home/TopRestaurant/TopRestaurant.js
+++ src/Pages/Home/TopRestaurant/TopRestaurant.js
@@ -41,7 +41,7 @@ const TopRestaurant = () => {
             <div className='grid  gap-6 grid-cols-1  md:grid-cols-2 lg:grid-cols-3 mx-16'>
 
                 {
-                    restaurants.map(restaurant => <TopRestaurantCard
+                    restaurants?.map(restaurant => <TopRestaurantCard
                         key={restaurant._id}
                         restaurant={restaurant}
                     ></TopRestaurantCard>)
--- src/Pages/RestaurantDetails/RestaurantDetails.js
+++ src/Pages/RestaurantDetails/RestaurantDetails.js
@@ -9,7 +9,7 @@ import AddToCartModal from '../Home/AllCategory/AddToCartModal/AddToCartModal';
 
 const RestaurantDetails = () => {
 
-    const[_id, title]=useLoaderData();
+    const [_id, title] = useLoaderData();
 
     const foods = useLoaderData();
     const resEmail = foods[0]?.resEmail;
@@ -47,14 +47,13 @@ const RestaurantDetails = () => {
         event.preventDefault();
         const form = event.target;
         const name = `${form.name.value}`;
-        const title = `${form.restaurant.value}`;
+        // const title = `${form.restaurant.value}`;
         // const email = user?.email || 'unregistered';
         const message = form.message.value;
 
         const review = {
             service: _id,
-            restaurantName: title,
-
+            restaurantName: restaurant.title,
             customer: name,
             // email,
             message,
@@ -81,17 +80,17 @@ const RestaurantDetails = () => {
 
 
     }
+    
     return (
         <div>
             <div
                 className="bg-fixed md:bg-auto bg-cover bg-center  py-64"
                 style={{ backgroundImage: `url(${restaurant.img})` }}
             >
-
             </div>
 
             <div>
-                {/* <img className='h-2/4' src={restaurants.img} alt="" /> */}
+
                 <h1 className="text-3xl font-bold mx-5 mt-3">{restaurant.title}</h1>
 
             </div>
@@ -99,53 +98,65 @@ const RestaurantDetails = () => {
                 <HiLocationMarker className='mt-1 text-3xl text-red-800'></HiLocationMarker>
                 <p className='text-3xl'>{restaurant.location} </p>
             </div>
+
+
             {/* review */}
-            <h1 className="text-3xl text-center">Write A Review</h1>
-            <form onSubmit={handlePlaceReview} className="w-96 mx-auto mt-5">
+            <div className='flex gap-5 flex-cols-1 md:flex-1 lg:flex-3 m-8 '>
+                <div className='w-1/4 text-center shadow-2xl'>
+                    <div>
+                    <h1 className='text-2xl text-yellow-400 font-semibold mb-3'>Restaurant Name & Total Food </h1>
+                    </div>
+                    <h1 className='text-xl'>Restaurant name: {restaurant.title}</h1>
+                    <p className='text-xl'>Tolal foods : {foods?.length}</p>
+                    <div className='mt-8'>
+                        <h1 className="text-3xl mb-3">Advertisement</h1>
+                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/03/sd.png" alt="" />
+                    </div>
+                </div>
+                <div class="w-1/2  text-center">
+                    <h1 className="text-3xl text-yellow-400 font-semibold mb-5">All Items</h1>
+                    <div className="mt-15 ">
+                        {foods?.map((item, i) => (
+                            <AllCategoryDetails key={i} item={item} handleCartModal={handleCartModal}></AllCategoryDetails>
+                        ))}
+                    </div>
+                    <AddToCartModal foodItem={foodItem} itemQuantity={itemQuantity} handleIncreaseQuantity={handleIncreaseQuantity}> handleDecreaseQuantity={handleDecreaseQuantity}</AddToCartModal>
 
-                <div className="mt-5">
+                </div>
+                <div className='w-1/4 text-center'>
+                    {/* <h2>Right side</h2> */}
+                    <h1 className="text-3xl text-center text-yellow-400">Write A Review</h1>
+                    <form onSubmit={handlePlaceReview} className="w-96 mx-auto mt-5">
 
-                    <div className="mx-auto mt-5">
+                        <div className="mt-5">
 
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <div className="card-body">
-                                <div className="form-control">
+                            <div className="mx-auto mt-5">
 
-                                    <input name='name' type="text" placeholder=" Name" className="input input-bordered" />
-                                </div>
+                                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
+                                    <div className="card-body">
+                                        <div className="form-control">
 
-                                {/* <div className="form-control">
-                                <input name='restaurant' type="text" placeholder=" restaurantName" className="input input-bordered" />
+                                            <input name='name' type="text" placeholder=" Name" className="input input-bordered" />
+                                        </div>
 
-                                </div> */}
-
-                                <div className="form-control">
-
-                                    {/* <input name="restaurants" type="text" placeholder="Restaurants Name" defaultValue={name} className="input input-ghost " /> */}
-
-                                </div>
-                                <textarea name="message" className="textarea textarea-bordered h-24 w-full" placeholder="text your message"></textarea>
-                                <div className="form-control mt-6">
-                                    <button className="btn btn-primary">Add your review </button>
+                                        <textarea name="message" className="textarea textarea-bordered h-24 w-full" placeholder="text your message"></textarea>
+                                        <div className="form-control mt-6">
+                                            <button className="btn btn-primary">Add your review </button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+
+                    </form>
+
                 </div>
 
-            </form>
-
-            <h1>Restaurant name: {restaurant.title}</h1>
-            Tolal foods : {foods?.length}
-
-            <div className="mt-15">
-                {foods?.map((item, i) => (
-                    <AllCategoryDetails key={i} item={item} handleCartModal={handleCartModal}></AllCategoryDetails>
-                ))}
             </div>
-            <AddToCartModal foodItem={foodItem} itemQuantity={itemQuantity} handleIncreaseQuantity={handleIncreaseQuantity}> handleDecreaseQuantity={handleDecreaseQuantity}</AddToCartModal>
-
         </div>
+
+
+
     );
 };
 
