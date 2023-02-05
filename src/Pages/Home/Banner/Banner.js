06f0b76ae2281c3b454ba5436a3e2436b84b6507
JubaStriker <jubastriker@gmail.com>
2023-02-05 20:53:02 +0600

    banner edited

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,13 +4,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import food3 from '../../../Assets/homepage-banner-img/7.jpg'
-import food4 from '../../../Assets/homepage-banner-img/2.jpg'
-import food5 from '../../../Assets/homepage-banner-img/3.jpg'
-import food6 from '../../../Assets/homepage-banner-img/4.jpg'
-import food7 from '../../../Assets/homepage-banner-img/5.jpg'
-import food8 from '../../../Assets/homepage-banner-img/6.jpg'
-import food1 from '../../../Assets/homepage-banner-img/1.jpg'
+import food3 from '../../../Assets/homepage-banner-img/9.png'
+import food4 from '../../../Assets/homepage-banner-img/2.png'
+import food5 from '../../../Assets/homepage-banner-img/3.png'
+import food6 from '../../../Assets/homepage-banner-img/4.png'
+import food7 from '../../../Assets/homepage-banner-img/5.png'
+import food8 from '../../../Assets/homepage-banner-img/6.png'
+import food1 from '../../../Assets/homepage-banner-img/8.png'
+import food2 from '../../../Assets/homepage-banner-img/7.png'
+import food9 from '../../../Assets/homepage-banner-img/1.png'
 
 
 const Banner = () => {
@@ -30,13 +32,15 @@ const Banner = () => {
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide><img src={food1} alt="banner" className='mx-auto' /></SwiperSlide>
-                <SwiperSlide><img src={food3} alt="banner" className='mx-auto' /></SwiperSlide>
-                <SwiperSlide><img src={food4} alt="banner" className='mx-auto' /></SwiperSlide>
-                <SwiperSlide><img src={food5} alt="banner" className='mx-auto' /></SwiperSlide>
-                <SwiperSlide><img src={food6} alt="banner" className='mx-auto' /></SwiperSlide>
-                <SwiperSlide><img src={food7} alt="banner" className='mx-auto' /></SwiperSlide>
-                <SwiperSlide><img src={food8} alt="banner" className='mx-auto' /></SwiperSlide>
+                <SwiperSlide><img src={food1} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food2} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food3} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food4} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food5} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food6} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food7} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food8} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
+                <SwiperSlide><img src={food9} alt="banner" className='mx-auto h-[900px] w-full' /></SwiperSlide>
             </Swiper>
         </>
 
