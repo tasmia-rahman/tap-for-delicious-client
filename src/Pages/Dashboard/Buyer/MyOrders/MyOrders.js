09a938184afb9b80bf1dc9bf804066801e0d1212
tasmia-rahman <maliha1288@gmail.com>
2023-02-08 22:27:34 +0600

    custom hook updated

--- src/Pages/Dashboard/Buyer/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/Buyer/MyOrders/MyOrders.js
@@ -7,7 +7,7 @@ import Loading from './../../../Shared/Loading/Loading';
 const MyOrders = () => {
 
     const { user } = useContext(AuthContext);
-    const { buyer } = useUser(user?.displayName);
+    const { buyer } = useUser(user?.email);
     console.log(buyer);
 
     const { data: myOrders = [], isFetching } = useQuery({
