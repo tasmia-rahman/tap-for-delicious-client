7d0d9e373795df3f909dc02e804f131e820a2ae7
JubaStriker <jubastriker@gmail.com>
2023-02-14 00:21:17 +0600

    design fixes

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -3,10 +3,7 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="min-h-screen bg-base-100">
-            <div className="text-center pt-10">
-                {/* <h1 className="font-semibold text-lg">Loading...</h1> */}
-            </div>
+        <div className="min-h-screen bg-base-100 mx-auto my-auto">
             <div className="max-w-[400px] mx-auto my-auto">
                 <Player
                     src='https://assets3.lottiefiles.com/packages/lf20_p8bfn5to.json'
