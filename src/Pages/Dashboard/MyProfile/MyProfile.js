bc3b7a3744f6b13452ecc638555a02d46da98d55
JubaStriker <jubastriker@gmail.com>
2023-02-17 12:35:14 +0600

    error fixed

--- src/Pages/Dashboard/MyProfile/MyProfile.js
+++ src/Pages/Dashboard/MyProfile/MyProfile.js
@@ -14,7 +14,7 @@ const MyProfile = () => {
     const [userData, setUserData] = useState({});
 
     useEffect(() => {
-        fetch(`http://localhost:5000/user/${user?.email}}`)
+        fetch(`http://localhost:5000/user/${user?.email}`)
             .then(res => res.json())
             .then(data => setUserData(data))
     }
